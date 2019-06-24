a54008057e599fffa48855ee420d8f0ff80140f5
Silzhar <luis.ruizfer@gmail.com>
2019-06-24 21:16:10 +0200

    morse translator

--- KeepCoding_Bootcamp_3º_edicion/morse/morse_translator.py
+++ KeepCoding_Bootcamp_3º_edicion/morse/morse_translator.py
@@ -21,6 +21,8 @@ class Translator(ttk.frame):
         self.receiver_entry = ttk.Entry(self, width=16, textvariable=self.receiver)
         self.receiver_entry.place(x=318, y=10)
         
+        self.origin_text.blind('<KeyRelease>'self.modificaOrigin)
+
         self.origin_lbl = ttk.Label(self, text="Plano", width=5, anchor=W)
         self.origin_lbl.place(x=12, y=40)
         self.origin_text = Text(self, width=26,height=8)
@@ -40,24 +42,32 @@ class Translator(ttk.frame):
         btn_traduce = ttk.Button(self,command=self.traduce, text="Traduce")
         btn_traduce.place(y=115, x=500)
 
+    def modificaOrigin(self, e):
+        self.traduce()
+
     def traduce(self):
-        texto_original = self.origin_text.get(1)
+        texto_original = self.origin_text.get("1.0","end-1c")
         if self.traduccionDirecta:
             traduccion = morse.toMorse(texto_original)
         else:
             traduccion = morse.toPlain(texto_original)
-        print(traduccion)
+        self.destino_text.delete("1.0", END)
+        self.destino_text.insert(INSERT, traduccion)
+        
 
     def send_telegram(self):
         print("Enviar telegrama")
 
     def changeText(self):
+        self.origin_text.delete("1.0", END)
         if self.traduccionDirecta:
             self.origin_lbl.config(text="Morse")
             self.destino_lbl.config(text="Plano")
         else:
             self.origin_lbl.config(text="Plano")
             self.destino_lbl.config(text="Morse")
+
+        self.traduccionDirecta = not self.traduccionDirecta
 
 
 class MainApp(tk): # tk para crear ventana (hereda de tk)
