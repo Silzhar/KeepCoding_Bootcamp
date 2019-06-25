6e96c454913f19985e4863562fef66045fe708ee
Silzhar <luis.ruizfer@gmail.com>
2019-06-26 00:40:17 +0200

    calculadora .2

---
+++ KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_5_operaciones-aritmeticas.py
@@ -0,0 +1,13 @@
+numero1 = input("Primer valor :")
+numero2 = input("Segundo valor :")
+
+if numero1.isdigit() == True and numero2.isdigit() == True:
+    numero1 = int(input)
+    numero2 = int(input)
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    producto = numero1 * numero2
+    division = numero1 / numero2
+else:
+    numero1 = input("Primer valor")
+    numero2 = input("Segundo valor")
