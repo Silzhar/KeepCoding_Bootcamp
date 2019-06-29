d46090649801b33f48f29d09eb7f43d877183053
Silzhar <luis.ruizfer@gmail.com>
2019-06-29 19:03:59 +0200

    ejercicio 5. Pruebas

--- KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_5_operaciones-aritmeticas.py
+++ KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_5_operaciones-aritmeticas.py
@@ -2,29 +2,65 @@ numero1 = input("Primer valor :")
 numero2 = input("Segundo valor :")
 
 def validar(val1, val2):
-    if val1.isdigit() == True and val2.isdigit() == True:
+    while val1.isdigit() == int and val2.isdigit() == int:
         val1 = int(val1)
         val2 = int(val2) 
         
-    elif val1.isdigit() == False:
+    if val1.isalpha():
         val1 = input("Error,inserte un valor :")
-    elif val2.isdigit() == False:
+        val1 = int(val1)
+        val1 = numero1
+        return val1
+    
+    if val2.isalpha():
         val2 = input("Error,inserte un valor :")
+        val2 = int(val2)
+        val2 = numero2
+        return val2
 
-    return int(val1), int(val2)
+
+    return val1, val2
 
 
 
 if validar(numero1, numero2):
-    numero1 = int(numero1)
-    numero2 = int(numero2)
-    suma = numero1 + numero2
-    resta = numero1 - numero2
-    producto = numero1 * numero2
-    division = numero1 / numero2
+    valor1 = int(numero1)
+    valor2 = int(numero2)
+    suma = valor1 + valor2
+    resta = valor1 - valor2
+    producto = valor1 * valor2
+    division = valor1 / valor2
 
     
-    print(":::::::::")
+    print(":::::::::::::::::::::::::")
     print("Suma :{} Resta :{} Producto :{} División :{} ".format(round(suma,2),round(resta,2),round(producto,2),round(division,2)))
 
 
+    '''
+    if val1.isdigit() == int and val2.isdigit() == int:
+        val1 = int(val1)
+        val2 = int(val2) 
+        
+    if val1.isdigit() == False:
+        val1 = input("Error,inserte un valor :")
+        val1 = int(val1)
+    if val2.isdigit() == False:
+        val2 = input("Error,inserte un valor :")
+        val2 = int(val2)
+
+    return val1, val2
+
+
+
+def validar(val1, val2):
+    while val1.isdigit() == int and val2.isdigit() == int:
+            try:
+            val1 = int(val1)
+            val2 = int(val2)
+        except:
+            val1 = input("Error,inserte un valor :")
+            val2 = input("Error,inserte un valor :")
+
+        return val1, val2
+
+    '''
