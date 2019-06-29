16c5e37d52b044ec27acc36713eddffe4a79449a
Silzhar <luis.ruizfer@gmail.com>
2019-06-29 09:27:22 +0200

    ejercicio 5. Solucion

--- KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_5_operaciones-aritmeticas.py
+++ KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_5_operaciones-aritmeticas.py
@@ -3,33 +3,28 @@ numero2 = input("Segundo valor :")
 
 def validar(val1, val2):
     if val1.isdigit() == True and val2.isdigit() == True:
-        val1 = int
-        val2 = int
+        val1 = int(val1)
+        val2 = int(val2) 
         
     elif val1.isdigit() == False:
         val1 = input("Error,inserte un valor :")
     elif val2.isdigit() == False:
         val2 = input("Error,inserte un valor :")
 
-validar(numero1, numero2)
-
-suma = numero1 + numero2
-resta = numero1 - numero2
-producto = numero1 * numero2
-division = numero1 / numero2
-
-if validar == True :
-    print("Suma :"+suma+"Resta :"+resta+"Producto :"+producto+"División :"+division)
+    return int(val1), int(val2)
 
 
 
-'''
-if numero1.isdigit() == True and numero2.isdigit() == True:
-    numero1 = int
-    numero2 = int
-    print("Suma :",suma,"Resta :", resta,"Producto :",producto,"División :",division)
+if validar(numero1, numero2):
+    numero1 = int(numero1)
+    numero2 = int(numero2)
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    producto = numero1 * numero2
+    division = numero1 / numero2
 
-else:
-    numero1 = input("Primer valor")
-    numero2 = input("Segundo valor")
-    '''
+    
+    print(":::::::::")
+    print("Suma :{} Resta :{} Producto :{} División :{} ".format(round(suma,2),round(resta,2),round(producto,2),round(division,2)))
+
+
