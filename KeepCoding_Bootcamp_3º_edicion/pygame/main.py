16702be6954be04ebde89e6fbae759c67a9f8133
Silzhar <luis.ruizfer@gmail.com>
2019-07-01 23:46:30 +0200

    main.py - fase 2-

--- KeepCoding_Bootcamp_3º_edicion/pygame/main.py
+++ KeepCoding_Bootcamp_3º_edicion/pygame/main.py
@@ -34,6 +34,18 @@ class Ball(pg.Surface):
         self.x += self.dirx
         self.y += self.diry
 
+    def comprobarChoque(self, candidata):
+
+       if (candidata.x >= self.x and candidata.x <= self.x+16 or \
+          candidata.x+16 >= self.x and candidata.x+16 <= self.x+16) and \
+          (candidata.y >= self.y and candidata.y <= self.y+16 or \
+           candidata.y+16 >= self.y and candidata.y+16 <= self.y+16):
+
+           self.dirx = self.dirx * -1
+           self.x += self.dirx
+
+
+
 class Game:
     clock = pg.time.Clock()
 
@@ -45,7 +57,23 @@ class Game:
         self.display.set_caption('Mi juego')
 
         self.balls = []
+        
 
+
+        self.ball1 = Ball()
+        self.ball1.setColor((255, 0, 0))
+        self.ball1.x = random.randrange(800)
+        self.ball1.y = random.randrange(600)
+        self.ball1.velocidad = random.randrange(2, 9)
+
+        self.ball2 = Ball()
+        self.ball2.setColor((255, 255, 0))
+        self.ball2.x = random.randrange(800)
+        self.ball2.y = random.randrange(600)
+        self.ball2.velocidad = random.randrange(2, 9)
+
+
+        '''
         for i in range(5, 15):
             b = Ball()
             b.setColor((random.randrange(256), random.randrange(256), random.randrange(256)))
@@ -53,7 +81,7 @@ class Game:
             b.y = random.randrange(600)
             b.velocidad = random.randrange(10)
             
-            self.balls.append(b)
+            self.balls.append(b)   '''
 
         '''
         self.ball = Ball()
@@ -73,6 +101,20 @@ class Game:
                     pg.quit()
                     sys.exit()
 
+            self.ball1.avanza()
+            self.ball2.avanza()
+
+            self.ball1.comprobarChoque(self.ball2)
+            self.ball2.comprobarChoque(self.ball1)
+
+        #    if self.ball1.comprobarChoque(self.ball2):
+         #       print(" hay choque")
+
+            self.screen.fill((60,60,60))
+
+            self.screen.blit(self.ball1, (self.ball1.x , self.ball1.y))
+            self.screen.blit(self.ball2, (self.ball2.x , self.ball2.y))
+            '''
             #Modifica la posición de ball
             for ball in self.balls:
                 ball.avanza()
@@ -83,7 +125,7 @@ class Game:
                 self.screen.blit(self.balls[i],(self.balls[i].x,self.balls[i].y))
 
             for balls in self.balls:
-                self.screen.blit(balls, (balls.x , balls.y))
+                self.screen.blit(balls, (balls.x , balls.y))  '''
 
             self.display.flip()
 
