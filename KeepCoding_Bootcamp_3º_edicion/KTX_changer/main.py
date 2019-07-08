88585448f5df3485500a431df495c6b978d753c7
Silzhar <luis.ruizfer@gmail.com>
2019-07-08 23:11:12 +0200

    Changer : main / venv / config

--- KeepCoding_Bootcamp_3º_edicion/KTX_changer/main.py
+++ KeepCoding_Bootcamp_3º_edicion/KTX_changer/main.py
@@ -2,3 +2,24 @@ from tkinter import *
 from tkinter import ttk
 
 import configparser
+import json
+import requests
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+inSymbol = input('Moneda a convertir :')
+outSymbol = input('Moneda convertida :')
+
+url = config['fixer.io']['ALL_SYMBOLS_EP']
+api_key = config['fixer.io']['API_KEY']
+
+url = url.format(api_key, inSymbol, outSymbol)
+response = requests.get(url)
+
+if response.status_code == 200:
+    currencies = json.loads(response.text)
+    print(currencies)
+    print(currencies['symbols']['USD'])
+else:
+    print("Error en peticion ",response.status_code)
