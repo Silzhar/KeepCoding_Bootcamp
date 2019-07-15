90f88f6aa4b7794078ac9cb4835637ad34b688b8
Silzhar <luis.ruizfer@gmail.com>
2019-07-15 22:33:02 +0200

    añadir diccionarios

--- KeepCoding_Bootcamp_3º_edicion/flask/app/views.py
+++ KeepCoding_Bootcamp_3º_edicion/flask/app/views.py
@@ -1,12 +1,24 @@
 
 from app import app
 from flask import render_template
+import csv
+
+ficheromovimientos = 'data/movimientos.txt'
 
 @app.route('/')
 def index():
+    # leer movimientos
+    fMovimientos = open(ficheromovimientos, "r")
+    csvreader = csv.reader(fMovimientos, delimiter=',', quotechar='"')
+    movimientos = []
+    for movimiento in csvreader:
+        movimientos.append(movimiento)
+        
+    # enviarlos a index.html
+
     return render_template('index.html')
 
 @app.route('/adios')
 def second_page():
-    return render_template('adios.html')
+    return render_template('adios.html', datos_asociados)
     
