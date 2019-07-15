b9980cff703e657aa9fb356f8ecbba93f03fe6a3
Silzhar <luis.ruizfer@gmail.com>
2019-07-15 12:16:48 +0200

    ejercicio 9 - validación entrada -

--- KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_9_pintura_techo.py
+++ KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_9_pintura_techo.py
@@ -3,12 +3,44 @@ from math import ceil
 bote = 5
 metrosBote = 100
 
-anchoTecho = int(input("Introduzca el alcho del techo a pintar :"))
-fondoTecho = int(input("Introduzca el fondo del techo a pintar :"))
+anchoTecho = input("Introduzca el alcho del techo a pintar :")
+fondoTecho = input("Introduzca el fondo del techo a pintar :")
 
-metrosTecho = anchoTecho + fondoTecho
+
+def entradas(ancho, fondo):
+#    ancho = anchoTecho
+ #   fondo = fondoTecho
+
+    try:
+        ancho = int(ancho)
+        
+    except ValueError :
+        while ValueError :
+            ancho = input("Introduzca un número y que sea positivo para el ancho :")
+            if ancho.isdigit():
+                ancho = int(ancho)
+                break
+
+    try:
+        fondo = int(fondo)
+
+    except ValueError:
+        while ValueError :
+            fondo= input("Introduzca un número y que sea positivo para el fondo :")
+            if fondo.isdigit():
+                fondo = int(fondo)
+                break
+
+    return ancho, fondo
+    
+
+datos = entradas(anchoTecho, fondoTecho)
+
+
+metrosTecho = datos[0] + datos[1]        
 botesCompra = metrosTecho / metrosBote
 pintura = (metrosTecho / metrosBote) * bote
 
 print("Litros de pintura necesários : {}".format(round(pintura, 1)))
 print("botes a comprar : {}".format(ceil(botesCompra)))
+
