baa83117023190cd9fd2cf251bcfdeb297361fab
Silzhar <luis.ruizfer@gmail.com>
2019-07-28 13:38:41 +0200

    ejercicio 10 -validaciones-

--- KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_10_impuestos.py
+++ KeepCoding_Bootcamp_3º_edicion/ejercicios/ejercicio_10_impuestos.py
@@ -1,15 +1,39 @@
-totalObjetos = int(input("Ingrese el total de objetos a procesar :"))
+def objetos():
+    totalObjetos = input("Ingrese el total de objetos a procesar :")
+    
+    try:
+        totalObjetos = int(totalObjetos)
+    except ValueError:
+        while totalObjetos.isdigit() == False:
+            print("-- Debe ingresar un valor positivo --")
+            totalObjetos = input("Ingrese el total de objetos a procesar :")
+            if totalObjetos.isdigit():
+                totalObjetos = int(totalObjetos)
+                break
+
+    return totalObjetos
+
+totalObjetos = objetos() 
 
 
-
-def entradas():
+def entradas(Precios=[]):
     
     contador = 0
-    while contador <=totalObjetos:
-        entrada = int(input("Ingrese precio del producto :"))
-        contador += 1
+    while contador < totalObjetos:
+        entrada = input("Ingrese precio del producto :")
 
-    return entrada
+        try:
+            entrada = int(entrada)
+            Precios.append(entrada)
+            contador += 1
+        except ValueError:
+            print("-- Debe ingresar un valor positivo --")
+        
+        
+    return Precios
 
-totalEntradas = entradas()
+totalEntradas = entradas(Precios=[])
 
+
+
+print(totalEntradas)
